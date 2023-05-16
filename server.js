f191a00249bb9c3bf7c6c36e73e0c370311e5b82
CathelijneVisser <112855711+CathelijneVisser@users.noreply.github.com>
2023-05-16 16:23:36 +0200

    css

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-/* Losjes gebaseerd op https://socket.io/get-started/chat */
-
 import * as path from 'path'
 
 import { Server } from 'socket.io'
@@ -9,33 +7,30 @@ import express from 'express'
 const app = express()
 const http = createServer(app)
 const ioServer = new Server(http)
-const port = process.env.PORT || 4242
 
-// Serveer client-side bestanden
+
+
 app.use(express.static(path.resolve('public')))
 
-// Start de socket.io server op
+
 ioServer.on('connection', (client) => {
-  // Log de connectie naar console
+  console.log(client)
   console.log(`user ${client.id} connected`)
+  ioServer.emit(`user ${client.id} connected`)
 
-  // Luister naar een message van een gebruiker
   client.on('message', (message) => {
-    // Log het ontvangen bericht
     console.log(`user ${client.id} sent message: ${message}`)
-
-    // Verstuur het bericht naar alle clients
     ioServer.emit('message', message)
   })
 
-  // Luister naar een disconnect van een gebruiker
   client.on('disconnect', () => {
-    // Log de disconnect
     console.log(`user ${client.id} disconnected`)
   })
 })
 
-// Start een http server op het ingestelde poortnummer en log de url
-  http.listen(port, () => {
-    console.log('listening on http://localhost:' + port)
-  })
+
+
+const port = process.env.PORT || 4242
+http.listen(port, () => {
+  console.log('listening on http://localhost:' + port)
+})
